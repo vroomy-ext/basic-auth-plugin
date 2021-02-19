84a3cc85e5e6f7df22f4aa625babc7a747515042
Josh Montoya <josh@montoya.io>
2021-02-19 14:24:21 -0800

    implement realm

--- main.go
+++ main.go
@@ -13,23 +13,42 @@ func Init(env map[string]string) (err error) {
 
 // BasicAuth will provide a basic authentication layer
 func BasicAuth(args ...string) (h common.Handler, err error) {
-	if len(args) != 2 {
-		err = fmt.Errorf("invalid number of arguments, expected 2 and received %d", len(args))
+	var (
+		username string
+		password string
+		realm    string
+	)
+
+	switch len(args) {
+	case 2:
+		username = args[0]
+		password = args[1]
+
+	case 3:
+		username = args[0]
+		password = args[1]
+		realm = args[3]
+
+	default:
+		err = fmt.Errorf("invalid number of arguments, expected 2 or 3 and received %d", len(args))
 		return
 	}
 
-	username := args[0]
-	password := args[1]
-	h = newHandler(username, password)
+	h = newHandler(username, password, realm)
 	return
 }
 
-func newHandler(username, password string) common.Handler {
+func newHandler(username, password, realm string) common.Handler {
+	realm = fmt.Sprintf(`Basic realm="%s"`, realm)
 	return func(ctx common.Context) {
 		req := ctx.Request()
 		u, p, ok := req.BasicAuth()
 		switch {
 		case !ok:
+			w := ctx.Writer()
+			w.Header().Set("WWW-Authenticate", realm)
+			ctx.WriteString(401, "text/plain", "Please provide login credentials\n")
+			return
 		case u != username:
 		case p != password:
 
